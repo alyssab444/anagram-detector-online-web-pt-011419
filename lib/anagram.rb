b3f96dbeb21d598013681487f23826e32d1a727a
Alyssa Brown <alyssa.brown@live.com>
2019-02-10 16:36:12 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,12 @@
 # Your code goes here!
 class Anagram 
   attr_accessor :match 
+  #take a word on initialization 
+  #instances should respond to match method 
+  #match method takes an array of possible anagrams
+  #return all matches in an array 
+  #if no matches exist, it should return an empty array 
+  
 
 
 
